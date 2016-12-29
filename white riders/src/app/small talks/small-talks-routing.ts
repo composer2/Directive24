d1b937b9839e7c59f05714e0c94108cc8c79b43a
composer2 <composer2be@gmail.com>
2016-12-29 12:53:14 +0200

    fixed miss match route to small talks

--- white riders/src/app/small talks/small-talks-routing.ts
+++ white riders/src/app/small talks/small-talks-routing.ts
@@ -6,7 +6,7 @@ import { SmallTalksInterviewComponent } from './small-talks-interview.component'
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: SmallTalksComponent,
     children: [
       { path: '', component: SmallTalksInterviewComponent },
--- white riders/src/app/small talks/small-talks.module.ts
+++ white riders/src/app/small talks/small-talks.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { SmallTalksRouterModule, routedComponents } from './home-routing.module';
+import { SmallTalksRouterModule, routedComponents } from './small-talks-routing';
 
 @NgModule({
   imports: [CommonModule, FormsModule, SmallTalksRouterModule],
