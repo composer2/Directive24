e0302f8b59924696ca82008df74f08356300347f
HrisiCookie <hristina.bailova@gmail.com>
2016-12-30 18:32:31 +0200

    added login-form validation

--- white riders/src/app/register/login-form.component.ts
+++ white riders/src/app/register/login-form.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 import { Api } from '../shared/kinvey-api.service';
 import { User } from './user';
@@ -7,16 +8,30 @@ import { UserService } from './register.service';
 
 @Component({
     selector: 'login-form',
-    templateUrl: 'login-form.component.html'
+    templateUrl: './login-form.component.html'
 })
-export class LoginFormComponent {
-    model: any = {};
-    data: any = {}
+export class LoginFormComponent implements OnInit{
+    model: any = {
+        username: '',
+        password: ''
+    };
+
+    data: any = {};
+    public userToLogin: FormGroup;
 
     constructor(
         private router: Router,
-        private UserService: UserService
-    ) { }
+        private UserService: UserService,
+        fb: FormBuilder
+    ) {
+        this.userToLogin = fb.group({
+            'username': ['', Validators.compose([Validators.required, Validators.minLength(5)])],
+            'password': ['', Validators.compose([Validators.required, Validators.minLength(4)])]
+        });
+    }
+
+    ngOnInit() {
+    }
 
     login() {
         this.data = {
--- white riders/src/app/register/login.module.ts
+++ white riders/src/app/register/login.module.ts
@@ -1,21 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { UserService } from './register.service';
 // import { routedComponents, RegisterRoutingModule } from './register-routing.module';
-import { loginRoutedComponents, LoginRoutingModule} from './login-routing.module';
+import { loginRoutedComponents, LoginRoutingModule } from './login-routing.module';
 
 @NgModule({
-  imports: [CommonModule,
-            FormsModule,
-            // RegisterRoutingModule,
-            LoginRoutingModule
-  ],
-  declarations: [
-    //   routedComponents,
-  loginRoutedComponents
-  ],
-  providers: [UserService],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        LoginRoutingModule
+    ],
+    declarations: [
+        //   routedComponents,
+        loginRoutedComponents
+    ],
+    providers: [UserService],
 })
 export class LoginModule { }
